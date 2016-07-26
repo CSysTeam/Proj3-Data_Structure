c4c56b862453ec43575125726bf3b526cc0f0365
basma11elmihy <basma.elmihy@gmail.com>
2016-07-26 16:14:10 +0200

    this is a header file to a stack program

---
+++ stack.h
@@ -0,0 +1,17 @@
+#ifndef STACKH_H
+#define STACKH_H
+
+//Add an element to the top of the stack
+int push(int x);
+//Removes the top element of the stack
+int pop();
+//Returns the top element of the stack without removing it
+int peek();
+//Check if the stack is full
+int isFull();
+//Check if the stack is empty
+int isEmpty();
+
+#endif // STACKH_H
+
+
