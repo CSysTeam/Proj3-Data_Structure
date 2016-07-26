1b31a5d24fe88d96305465c982964d35be5ddfe0
basma11elmihy <basma.elmihy@gmail.com>
2016-07-26 15:52:19 +0200

    Add files via upload

---
+++ stack.c
@@ -0,0 +1,51 @@
+#include"stack.h"
+
+int top = -1;
+int data;
+int stack[5];
+int max = 5;
+
+int push(int data)
+{
+
+   if (isFull()==1)
+    return 0;
+   else
+    top = (top+1);
+    top = data;
+
+}
+int pop()
+{
+ if (!isFull())
+ {
+
+  top = data;
+  top = top-1;
+  return data;
+
+ }
+}
+ int isFull()
+ {
+
+     if (top == max)
+        return 1;
+     else
+        return 0;
+
+}
+int isEmpty()
+{
+    if (top == -1)
+        return 1;
+    else
+        return 0;
+
+}
+int peek()
+{
+    return stack[top];
+}
+
+
