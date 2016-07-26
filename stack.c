98b96b6952e08420cc830e292ffdb47072654057
basma11elmihy <basma.elmihy@gmail.com>
2016-07-26 17:07:26 +0200

    this is a stack program

--- stack.c
+++ stack.c
@@ -2,44 +2,27 @@
 
 int top = -1;
 int data;
-int stack[8];
-int max = 8;
+int stack[];
+
 
 int push(int data)
 {
 
-   if (!isFull())
-
-
-   {
-    top = (top+1);
+    top = top+1;
     stack[top] = data;
 
-   }
-   else
-       printf("error::stack is full");
+
 
 }
 int pop()
 {
- if (!isFull())
- {
+
 
   data = stack[top];
   top = top-1;
   return data;
 
- }
- else
-     printf("Stack is empty nothing to pop");
-}
- int isFull()
- {
 
-     if (top == max)
-        return 1;
-     else
-        return 0;
 
 }
 int isEmpty()
